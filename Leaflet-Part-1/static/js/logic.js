7c35069d68cca8f92260bfd1b79c25adf1232cb6
Mark <markosowski2023@u.northwestern.edu>
2022-10-04 20:57:15 -0500

    part one

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,10 +1,44 @@
-var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+function createMap(earthquakeMap) {
 
-var earthquakes = L.layerGroup();
+    var streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
 
-var map = L.map("map").setView([37.7749, -122.4194], 13);
+    var baseMap = {
+        "Street Map": streetmap
+    };
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+    var overlayMaps = {
+        "Earthquake Maps": earthquakeMap
+    };
+
+    var map = L.map("map", {
+        center: [37.7749, -122.4194],
+        zoom: 5,
+        layers: [streetmap, earthquakeMap]
+    });
+
+    L.control.layers(baseMap, overlayMaps, {
+        collapsed: false
+      }).addTo(map);
+
+}
+
+function createMarkers(response) {
+
+    var earthquakes = response.metadata.properties;
+
+    var earthquakeMarkers = [];
+
+    for (var i = 0; i < earthquakes; i++) {
+        var earthquake = earthquakes[i];
+
+        var earthquakeMarker = L.marker([earthquake.mag]).bindPopup("<h3>" + earthquake.mag + "</h3>");
+
+        earthquakeMarkers.push(earthquakeMarker);
+    }
+
+    createMap(L.layerGroup(earthquakeMarkers));
+}
+
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(createMarkers);
