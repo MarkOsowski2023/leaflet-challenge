f2fe28b0507346d84355d1323045093f73af933f
Mark <markosowski2023@u.northwestern.edu>
2022-10-05 23:58:32 -0500

    almost

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -21,70 +21,76 @@ function createMap(earthquakeMap) {
     L.control.layers(baseMap, overlayMaps, {
         collapsed: false
       }).addTo(map);
+      legend.addTo(map);
 
 }
-
-function createMarkers(response) {
-
-    var earthquakes = response.metadata;
-
-    var earthquakeMarkers = [];
-
-    for (var i = 0; i < earthquakes; i++) {
-        var earthquake = earthquakes[i];
-
-        var earthquakeMarker = L.marker([earthquake.properties.geometry.type[0]]).bindPopup("<h3>" + earthquake.properties.geometry.type[0] + "</h3>");
-
-        earthquakeMarkers.push(earthquakeMarker);
-    }
-
-    createMap(L.layerGroup(earthquakeMarkers));
-}
-
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(createMarkers);
+// Features
 
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 d3.json(url, function(data) {
-    function style(feature) {
-        return {
-            opacity: 1,
-            fillOpacity: 1,
-            fillColor: getColor(feature.properties.mag),
-            color: "#000000",
-            radius: getRadius(feature.properties.mag),
-            stroke: true,
-            weight: 0.4
-        };
-
-         // set different color from magnitude
-    function getColor(magnitude) {
-        switch (true) {
-        case magnitude > 5:
-          return "#ea2c2c";
-        case magnitude > 4:
-          return "#ea822c";
-        case magnitude > 3:
-          return "#ee9c00";
-        case magnitude > 2:
-          return "#eecc00";
-        case magnitude > 1:
-          return "#d4ee00";
-        default:
-          return "#98ee00";
-        }
-      }
-      // set radiuss from magnitude
-        function getRadius(magnitude) {
-        if (magnitude === 0) {
-          return 1;
-        };
-    
-        return magnitude * 4;
-      };
-    
-    };
-
-    
-
+  createFeatures(data.features);
 });
+
+function createFeatures(earthquakeData) {
+  // Give each feature a popup describing the place and time of the earthquake
+  function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3> Where: " + feature.properties.place +
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" + "<br><h2> Magnitude: " + feature.properties.mag + "</h2>");
+  }
+
+  // Create a GeoJSON layer containing the features array on the earthquakeData object
+  function createCircleMarker(feature,latlng){
+    let options = {
+        radius:feature.properties.mag*5,
+        fillColor: chooseColor(feature.properties.mag),
+        color: chooseColor(feature.properties.mag),
+        weight: 1,
+        opacity: .8,
+        fillOpacity: 0.35
+    }
+    return L.circleMarker(latlng, options);
+}
+  
+  let earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature,
+    pointToLayer: createCircleMarker
+  });
+  
+  // Send earthquakes layer to the createMap function
+  createMap(earthquakes);
+}
+
+// Color circles based on mag
+function chooseColor(mag) {
+  switch(true) {
+      case (1.0 <= mag && mag <= 2.5):
+        return "#0071BC";
+      case (2.5 <= mag && mag <= 4.0):
+        return "#35BC00";
+      case (4.0 <= mag && mag <= 5.5):
+        return "#BCBC00";
+      case (5.5 <= mag && mag <= 8.0):
+        return "#BC3500";
+      case (8.0 <= mag && mag <= 20.0):
+        return "#BC0000";
+      default:
+        return "#E2FFAE";
+  }
+}
+
+let legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+    let div = L.DomUtil.create('div', 'info legend'),
+        grades = [1.0, 2.5, 4.0, 5.5, 8.0],
+        labels = [];
+
+    // loop through density intervals
+    for (let i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + chooseColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+    return div;
+};
